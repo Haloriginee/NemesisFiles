ace82731acbcf61915d64110435d4ddc22343170
Kevin Hang <herezy2006@gmail.com>
2025-06-10 10:18:38 +0200

    Game Start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+
+    <main>
+      <h1 className='text-5xl text-blue-500 font-bold'>
+        Welcome to the React App!
+      </h1>
+    </main>
+
+  )
+}
+
+export default App
