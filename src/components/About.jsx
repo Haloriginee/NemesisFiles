59efc5079d6370995ddc9c7def6fad50b116f6cb
Kevin Hang <herezy2006@gmail.com>
2025-06-11 21:06:37 +0200

    animation looks sexy

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
 import React from 'react'
+import AnimatedTitle from './AnimatedTitle'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -36,9 +37,9 @@ const About = () => {
         <h2 className='font-general text-sm uppercase md:text-[10px]'>
           Welcome to Zentry
         </h2>
-        <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem] '>
-          Disc<b>o</b>ver the world's <br /> L<b>a</b>rgest shared adventure
-        </div>
+
+        <AnimatedTitle title="Disc<b>o</b>ver the world's <br /> L<b>a</b>rgest shared adventure" containerClass="mt-5 !text-black text-center"/>
+
         <div className='about-subtext'>
           <p>
             The Game of Games begins your life, now an epic MMORPG
