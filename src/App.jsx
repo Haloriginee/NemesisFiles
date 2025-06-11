a820cdbbe8755317da7855de65cec07c1002e7c5
Kevin Hang <herezy2006@gmail.com>
2025-06-11 16:12:49 +0200

    Video Player and Style fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
 
-    <div className='relative h-dvh w-screen overflow-x-hidden'>
-      <div id="video-frame" className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
-        <div>
-          <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
-            Is this Working ?
-          </div>
-        </div>
-      </div>
-    </div>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+        <Hero/>
+    </main>
 
   )
 }
