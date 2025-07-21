7c1bcdcb2ea550555aeff62e68bdf5bc8ab3deb5
Kevin Hang <herezy2006@gmail.com>
2025-07-21 23:43:35 +0200

    ok

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -88,7 +88,7 @@ const Story = () => {
         <div className='-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end'>
           <div className='flex h-full w-fit flex-col items-center md:items-start'>
             <p className='mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start '>
-              Where realms converge, lies Zentry and the boudless pillar. Discover its screts and shape your fate amidst infinite opportunitites.
+              something here
             </p>
             <Button
               id='realm-button'
