248d4f7fe8980ec6d37de5fe17f1c7e8e56ac714
Kevin Hang <herezy2006@gmail.com>
2025-07-22 09:07:10 +0200

    ok

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -88,7 +88,7 @@ const Story = () => {
         <div className='-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end'>
           <div className='flex h-full w-fit flex-col items-center md:items-start'>
             <p className='mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start '>
-              something here
+              Where heavens crack and realms entwine, the ancient path reveals itself. Uncover forgotten truths and forge your fate within the boundless myth.
             </p>
             <Button
               id='realm-button'
