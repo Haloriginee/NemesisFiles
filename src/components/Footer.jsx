5a839e1ce7e910e8a87138ef8eea71bb56ba3468
Kevin Hang <herezy2006@gmail.com>
2025-07-31 22:20:14 +0200

    ok

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
     <footer className='w-screen bg-violet-300 py-4 text-black'>
       <div className='container mx-auto flex flex-full items-center justify-between gap-4 px-4 md:flex-row'>
         <p className='text-center text-small md:text-left'>
-          &copy; All rights reserved.
+          &copy; All rights reserved. okay
         </p>
 
         <div className='flex justify-center gap-4 md:justify-start'>
